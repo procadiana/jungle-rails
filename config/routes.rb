134b02ba04c3183b44458fa061264d3429708835
procadiana <diana.claudia.ilinca@gmail.com>
2019-05-16 18:58:41 +0000

    Register and Login

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,20 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
+
   end
 
   resources :orders, only: [:create, :show]
@@ -17,6 +25,8 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories
   end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
