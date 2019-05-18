268555ae22824bba13772e86afc1f6f708cdb1f3
procadiana <diana.claudia.ilinca@gmail.com>
2019-05-18 01:19:46 +0000

    Receipt html

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,9 @@
 class UserMailer < ApplicationMailer
+
+
   def order_receipt (order)
     @url  = 'http://jungle.com/login'
-
+    @order = order
     mail(to: order.email, subject: 'Your Order' + order.id.to_s)
   end
 end
